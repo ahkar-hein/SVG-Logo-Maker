b51e9305b0735241230efc4629e42b3f1e535916
Ahkar Hein <ahkarhein04@gmail.com>
2023-07-27 21:53:26 -0700

    added prompt question for user into questions array

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const fs = require('fs');
 const Circle = require('./lib/circle.js');
 const Triangle = require('./lib/triangle.js');
 const Square = require('./lib/square.js');
+
+const logoFilePath = './logo.svg';
+const questions = [
+    {
+        type: 'input',
+        name: 'logoName',
+        message: 'Enter text for the logo. (No more than 3 letters)',
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Enter text color for the logo.',
+    },
+    {
+        type: 'list',
+        name: 'shapeType',
+        message: 'Select shape for Logo.',
+        choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color for the logo.',
+    },
+];
