4863b7ab818a08b8542298161dd64bc5a931c582
Ahkar Hein <ahkarhein04@gmail.com>
2023-07-27 21:29:46 -0700

    create shape class in shape.js

---
+++ lid/shape.js
@@ -0,0 +1,9 @@
+class Shape {
+    constructor(shapeColor) {
+        this.shapeColor = shapeColor;
+    }
+      setColor(shapeColor) {
+        this.shapeColor = shapeColor;
+      }
+    }
+module.exports = Shape;
