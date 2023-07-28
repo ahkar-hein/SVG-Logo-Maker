60357efcedd3a7d4b73fa05e641eba17e43ccfe3
Ahkar Hein <ahkarhein04@gmail.com>
2023-07-27 21:48:32 -0700

    import require fs and inquirer and circle, triange and square js

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Circle = require('./lib/circle.js');
+const Triangle = require('./lib/triangle.js');
+const Square = require('./lib/square.js');
